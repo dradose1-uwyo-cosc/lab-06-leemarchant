714cad505241b321f94c162e61976569d27c9240
leemarchant <mjadalie@gmail.com>
2024-10-19 19:36:59 +0000

    completed lab assignment

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -1,13 +1,10 @@
-# Your Name Here
+# Lee Marchant
 # UWYO COSC 1010
-# Submission Date
+# 10/19/24
 # Lab 06
-# Lab Section: 
-# Sources, people worked with, help given to: 
-# your
-# comments
-# here
-
+# Lab Section: 14
+# Chat-GPT-3. (2024, October 19). “Can you give me an example of assigning a new instance of a string letter from a string to a dictionary in python?” Generated using OpenAI. https://chat.openai.com/
+# Chat-GPT-3. (2024, October 19). “Can you give me an example of comparing values of dictionary and then assign key based on value? Without using the float function.” Generated using OpenAI. https://chat.openai.com/
 
 random_string = """
 jppamiqxegokaizvkyawwurhewtcxohryzptznyuedhhmawpic
@@ -83,18 +80,44 @@ print(len(random_string)) # Print out the size for reference
 #Load all the elements into a dictionary
 #Will need to first declare a dictionary 
 
+letters_occurrence = {}
+
+for letter in sorted(random_string):
+    if letter in letters_occurrence:
+        letters_occurrence[letter] += 1
+    else:
+        letters_occurrence[letter] = 1
+
+print(f'Occurence of letters:')
+for key, value in letters_occurrence.items():
+    print(f'{key}: {value}')
+
 # Output: each letter and its corresponding occurrence in alphabetical order
 
-print("*"*75)
+##print("*"*75)
 # Output which letter occurred the most 
 
 most_occurred = ""
 least_occurred = ""
 
+for letter, occurrence in letters_occurrence.items():
+    if occurrence == max(letters_occurrence.values()):
+        most_occurred = letter
+    elif occurrence == min(letters_occurrence.values()):
+        least_occurred = letter
+
 print(f"The letter that occurred the most is {most_occurred}")
-print("*"*75)
+##print("*"*75)
 # Output which letter occurred the least 
-print(f"The letter that occurred the most is {least_occurred}")
-print("*"*75)
+print(f"The letter that occurred the least is {least_occurred}")
+##print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
+letters_percentage = {}
+
+for letter, occurrence in letters_occurrence.items():
+    letters_percentage[letter] = (occurrence/2500)*100
+
+print(f'Percentage of letter occurrence:')
+for key, value in letters_percentage.items():
+    print(f'{key}: {value}')
